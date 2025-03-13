936ec634f8d596e21f744db34878c6df47715403
Shouked <136965203+Shouked@users.noreply.github.com>
2025-03-13 14:02:42 -0300

    Update User.js

--- models/User.js
+++ models/User.js
@@ -3,8 +3,14 @@ const mongoose = require('mongoose');
 const userSchema = new mongoose.Schema({
   name: { type: String, required: true },
   phone: { type: String, required: true },
-  address: { type: String, required: true },
-  complement: { type: String },
+  address: {
+    cep: { type: String, required: true },
+    street: { type: String, required: true },
+    number: { type: String, required: true },
+    neighborhood: { type: String, required: true },
+    city: { type: String, required: true },
+    complement: { type: String },
+  },
   email: { type: String, required: true, unique: true },
   password: { type: String, required: true },
   createdAt: { type: Date, default: Date.now },
