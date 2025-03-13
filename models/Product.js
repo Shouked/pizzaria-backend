b7cd3d31e32017159f61ea4bf2d3f90de407e320
Shouked <136965203+Shouked@users.noreply.github.com>
2025-03-13 16:58:30 -0300

    Update Product.js

--- models/Product.js
+++ models/Product.js
@@ -2,10 +2,10 @@ const mongoose = require('mongoose');
 
 const productSchema = new mongoose.Schema({
   name: { type: String, required: true },
-  category: { type: String, required: true }, // ex.: "pizza", "sobremesa"
-  price: { type: Number, required: true },
   description: { type: String },
-  image: { type: String }, // URL da imagem
+  price: { type: Number, required: true },
+  category: { type: String, required: true },
+  image: { type: String },
 });
 
 module.exports = mongoose.model('Product', productSchema);
