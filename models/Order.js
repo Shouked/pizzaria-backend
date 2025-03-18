e89b03337d7e3e38784a388fd33a9d9b3ba36dac
Shouked <136965203+Shouked@users.noreply.github.com>
2025-03-18 15:27:53 -0300

    Update Order.js

--- models/Order.js
+++ models/Order.js
@@ -9,6 +9,15 @@ const orderSchema = new mongoose.Schema({
   user: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true },
   items: [itemSchema],
   total: { type: Number, required: true },
+  deliveryOption: { type: String, required: true, enum: ['delivery', 'pickup'] }, // Novo campo
+  address: { // Novo campo, opcional
+    cep: { type: String },
+    street: { type: String },
+    number: { type: String },
+    neighborhood: { type: String },
+    city: { type: String },
+    complement: { type: String },
+  },
   createdAt: { type: Date, default: Date.now },
 });
 
