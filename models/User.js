1b4a023847688afeb33b1daccbeaf865eb00cedb
Shouked <136965203+Shouked@users.noreply.github.com>
2025-03-21 09:00:24 -0300

    Update User.js

--- models/User.js
+++ models/User.js
@@ -14,6 +14,7 @@ const userSchema = new mongoose.Schema({
   },
   email: { type: String, required: true, unique: true },
   password: { type: String, required: true },
+  tenantId: { type: String }, // Adicionado como opcional
   createdAt: { type: Date, default: Date.now },
 });
 
