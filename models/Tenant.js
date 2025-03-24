f1f59afd0435bb4e664f8dfa8aa4f986a462f716
Shouked <136965203+Shouked@users.noreply.github.com>
2025-03-24 08:44:57 -0300

    Atualizar o Tenant.js

--- models/Tenant.js
+++ models/Tenant.js
@@ -1,18 +1,11 @@
-// models/Tenant.js
 const mongoose = require('mongoose');
 
 const TenantSchema = new mongoose.Schema({
-  name: {
-    type: String,
-    required: true
-  },
+  name: { type: String, required: true },
   logoUrl: String,
   primaryColor: String,
   secondaryColor: String,
-  createdAt: {
-    type: Date,
-    default: Date.now
-  }
+  createdAt: { type: Date, default: Date.now }
 });
 
 module.exports = mongoose.model('Tenant', TenantSchema);
