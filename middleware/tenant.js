4bb44fb2825690e49ab3bc1330ef7924605f0c81
Shouked <136965203+Shouked@users.noreply.github.com>
2025-03-24 21:55:00 -0300

    Atualizar o tenant.js

--- middleware/tenant.js
+++ middleware/tenant.js
@@ -13,24 +13,23 @@ const tenantMiddleware = async (req, res, next) => {
     console.log('tenantId:', tenantId);
 
     if (!tenantId) {
-      console.log('Nenhum tenantId encontrado!');
+      console.log('❌ Nenhum tenantId fornecido');
       return res.status(400).json({ message: 'Tenant ID is required' });
     }
 
-    const tenant = await Tenant.findOne({ tenantId });
-
-    console.log('Tenant encontrado:', tenant);
+    const tenant = await Tenant.findOne({ tenantId: tenantId });
 
     if (!tenant) {
-      console.log('Tenant não encontrado no banco.');
+      console.log('❌ Tenant não encontrado no banco');
       return res.status(404).json({ message: 'Tenant not found' });
     }
 
+    console.log('✅ Tenant encontrado:', tenant);
+
     req.tenant = tenant;
     next();
-
   } catch (error) {
-    console.error('Erro no tenantMiddleware:', error);
+    console.error('❌ Erro no tenantMiddleware:', error);
     res.status(500).json({ message: 'Server error in tenant middleware' });
   }
 };
