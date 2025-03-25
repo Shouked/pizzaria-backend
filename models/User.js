f060a50ccac41d421cce8d0ef6a9babfcfbcdf0f
Shouked <136965203+Shouked@users.noreply.github.com>
2025-03-25 17:14:41 -0300

    Atualizar o User.js

--- models/User.js
+++ models/User.js
@@ -1,19 +1,40 @@
 const mongoose = require('mongoose');
 
+const AddressSchema = new mongoose.Schema({
+  street: String,
+  number: String,
+  neighborhood: String,
+  city: String,
+  state: String,
+  zip: String,
+}, { _id: false });
+
 const UserSchema = new mongoose.Schema({
   tenantId: {
-    type: String, //  ALTERADO de ObjectId para String
+    type: String,
     required: true
   },
-  name: String,
+  name: {
+    type: String,
+    required: true,
+    trim: true
+  },
   email: {
     type: String,
-    unique: false,
-    required: true
+    required: true,
+    unique: false, // pode haver mesmo email em tenants diferentes
+    lowercase: true,
+    trim: true
   },
   password: {
     type: String,
     required: true
+  },
+  phone: {
+    type: String
+  },
+  address: {
+    type: AddressSchema
   },
   isAdmin: {
     type: Boolean,
