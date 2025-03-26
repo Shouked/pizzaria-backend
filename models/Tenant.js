b489ffb0195d71d4ad5d020df4d074e43cdf1f32
Shouked <136965203+Shouked@users.noreply.github.com>
2025-03-26 07:02:50 -0300

    Atualizar o Tenant.js

--- models/Tenant.js
+++ models/Tenant.js
@@ -1,6 +1,7 @@
 const mongoose = require('mongoose');
 
 const TenantSchema = new mongoose.Schema({
+  tenantId: { type: String, required: true, unique: true }, // Adicionado
   name: { type: String, required: true },
   logoUrl: String,
   primaryColor: String,
