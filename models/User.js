43faf64841893b44e1c3994d67fc5e71b4b5a183
Shouked <136965203+Shouked@users.noreply.github.com>
2025-03-26 10:50:36 -0300

    Update User.js

--- models/User.js
+++ models/User.js
@@ -8,37 +8,15 @@ const AddressSchema = new mongoose.Schema({
 const UserSchema = new mongoose.Schema({
   tenantId: {
     type: String,
-    required: true
+    required: function() { return !this.isSuperAdmin; } // Obrigatório apenas se não for superadmin
   },
-  name: {
-    type: String,
-    required: true,
-    trim: true
-  },
-  email: {
-    type: String,
-    required: true,
-    lowercase: true,
-    trim: true
-  },
-  password: {
-    type: String,
-    required: true
-  },
-  phone: {
-    type: String
-  },
-  address: {
-    type: AddressSchema
-  },
-  isAdmin: {
-    type: Boolean,
-    default: false
-  },
-  isSuperAdmin: {
-    type: Boolean,
-    default: false
-  }
+  name: { type: String, required: true, trim: true },
+  email: { type: String, required: true, lowercase: true, trim: true },
+  password: { type: String, required: true },
+  phone: { type: String },
+  address: { type: AddressSchema },
+  isAdmin: { type: Boolean, default: false },
+  isSuperAdmin: { type: Boolean, default: false }
 }, { timestamps: true });
 
 module.exports = mongoose.model('User', UserSchema);
